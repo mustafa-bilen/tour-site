518867d0bcc55bba985ee9274125b122045b6bf9
mustafa-bilen <mustafabilen6669@gmail.com>
2022-10-07 21:35:37 +0300

    close to final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./scss/App.scss";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Card from "./components/Card";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Card data={data} />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import "../scss/Card.scss";
+
+const Card = ({ data }) => {
+  return (
+    <div className="container">
+      {data.map((item) => {
+        const { id, title, desc, image } = item;
+        return (
+          <div className="content" key={id}>
+            <h2>{title}</h2>
+            <img src={image} alt="img" width={400} />
+            <div className="desc">{desc}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Card;
